be9f70123e39f1dbd8d35a996c736957b93255d8
Jacobo Tabernero Rey <jacoborus@gmail.com>
2022-04-13 23:32:12 +0200

    First boilerplate

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = defineProps<{
+  initial: number;
+}>();
+
+const count = ref(props.initial);
+const inc = () => count.value++;
+const dec = () => count.value--;
+</script>
+
+<template>
+  <div>
+    {{ count }}
+    <button class="inc" @click="inc()">+</button>
+    <button class="dec" @click="dec()">-</button>
+  </div>
+</template>
